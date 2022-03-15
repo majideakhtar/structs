a51031c78bb75071d0caf1d1e5356e7cb4ec48a6
Majid Equbal Akhtar <majid.equbal.akhtar@gmail.com>
2022-03-15 23:23:32 +0530

    Updating structs
    Signed-off-by: Majid Equbal Akhtar <majid.equbal.akhtar@gmail.com>

--- main.go
+++ main.go
@@ -6,5 +6,15 @@ type person struct {
 }
 
 func main() {
+	// alex := person{lastName: "Lima", firstName: "Alex"}
+	// fmt.Println(alex)
+	// alex.lastName = "Dickson"
+	// fmt.Println(alex)
+	// Another way to assogn structs
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Lima"
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex) // {firstName: lastName:}
 
 }
